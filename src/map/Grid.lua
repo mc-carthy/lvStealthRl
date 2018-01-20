eae0065a406c86e929ba66bb99b50428f8bb8199
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-01-20 22:16:27 +0000

    Scale celAut grid by 2 (is configurable) to world grid

--- src/map/Grid.lua
+++ src/map/Grid.lua
@@ -8,7 +8,9 @@ local gridDebugFlag = true
 local grid_rng = love.math.newRandomGenerator(os.time())
 
 local playerX, playerY = 0, 0
-local viewDist = 15
+local viewDistX = 33
+local viewDistY = 19
+local celAutGridScale = 2
 
 local _generateGrid = function(self)
     for x = 1, self.xSize do
@@ -21,15 +23,39 @@ local _generateGrid = function(self)
 end
 
 local _populateGrid = function(self)
-    local celAutGrid = CelAut.create(self.xSize, self.ySize, 485).grid
-    for x = 1, self.xSize do
-        for y = 1, self.ySize do
+    local celAutGrid = CelAut.create(self.xSize / celAutGridScale, self.ySize / celAutGridScale, 460).grid
+    for x = 1, self.xSize / celAutGridScale do
+        for y = 1, self.ySize / celAutGridScale do
             -- local prob = grid_rng:random(100)
             -- if prob >= 85 then
             --     self[x][y].walkable = false
             -- end
             if celAutGrid[x][y] then
-                self[x][y].walkable = false
+                if celAutGridScale == 1 then
+                    self[celAutGridScale * x][celAutGridScale * y].walkable = false
+                elseif celAutGridScale == 2 then
+                    self[celAutGridScale * x    ][celAutGridScale * y    ].walkable = false
+                    self[celAutGridScale * x - 1][celAutGridScale * y    ].walkable = false
+                    self[celAutGridScale * x    ][celAutGridScale * y - 1].walkable = false
+                    self[celAutGridScale * x - 1][celAutGridScale * y - 1].walkable = false
+                elseif celAutGridScale == 4 then
+                    self[celAutGridScale * x    ][celAutGridScale * y    ].walkable = false
+                    self[celAutGridScale * x - 1][celAutGridScale * y    ].walkable = false
+                    self[celAutGridScale * x - 2][celAutGridScale * y    ].walkable = false
+                    self[celAutGridScale * x - 3][celAutGridScale * y    ].walkable = false
+                    self[celAutGridScale * x    ][celAutGridScale * y - 1].walkable = false
+                    self[celAutGridScale * x - 1][celAutGridScale * y - 1].walkable = false
+                    self[celAutGridScale * x - 2][celAutGridScale * y - 1].walkable = false
+                    self[celAutGridScale * x - 3][celAutGridScale * y - 1].walkable = false
+                    self[celAutGridScale * x    ][celAutGridScale * y - 2].walkable = false
+                    self[celAutGridScale * x - 1][celAutGridScale * y - 2].walkable = false
+                    self[celAutGridScale * x - 2][celAutGridScale * y - 2].walkable = false
+                    self[celAutGridScale * x - 3][celAutGridScale * y - 2].walkable = false
+                    self[celAutGridScale * x    ][celAutGridScale * y - 3].walkable = false
+                    self[celAutGridScale * x - 1][celAutGridScale * y - 3].walkable = false
+                    self[celAutGridScale * x - 2][celAutGridScale * y - 3].walkable = false
+                    self[celAutGridScale * x - 3][celAutGridScale * y - 3].walkable = false
+                end
             end
         end
     end
@@ -64,10 +90,10 @@ local draw = function(self)
             -- end
             love.graphics.setColor(127, 127, 127)
             if not self[x][y].walkable then
-                love.graphics.setColor(0, 0, 0)
+                love.graphics.setColor(31, 31, 31)
             end
             if gridDebugFlag then
-                if x < viewDist + playerX and x > playerX - viewDist and y < viewDist + playerY and y > playerY - viewDist then
+                if x < viewDistX + playerX and x > playerX - viewDistX and y < viewDistY + playerY and y > playerY - viewDistY then
                     love.graphics.rectangle('fill', (x - 1) * self.cellSize, (y - 1) * self.cellSize, self.cellDrawSize, self.cellDrawSize)
                 end
             end
@@ -80,15 +106,15 @@ grid.create = function(entityManager)
 
     inst.tag = "grid"
     inst.entityManager = entityManager
-    inst.cellSize = 50
+    inst.cellSize = 20
     inst.worldScaleInScreens = 5
     local border = 0
     inst.cellDrawSize = inst.cellSize - border
     -- inst.xSize = love.graphics.getWidth() / inst.cellSize * inst.worldScaleInScreens
     -- inst.ySize = love.graphics.getHeight() / inst.cellSize * inst.worldScaleInScreens
 
-    inst.xSize = 150
-    inst.ySize = 150
+    inst.xSize = 210
+    inst.ySize = 210
     _generateGrid(inst)
     _populateGrid(inst)
 
