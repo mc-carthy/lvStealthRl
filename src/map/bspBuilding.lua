f88827edf90038e7f846af3aac29ccc0d1249006
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-01-30 19:08:25 +0000

    Only build a portion of building outer walls to allow player to enter

--- src/map/bspBuilding.lua
+++ src/map/bspBuilding.lua
@@ -1,13 +1,18 @@
 local bspBuilding = {}
 
+local bsp_rng = love.math.newRandomGenerator(os.time())
+
 local function createOuterWalls(self)
     local grid = {}
     for x = 1, self.w do
         grid[x] = {}
         for y = 1, self.h do
             grid[x][y] = {}
-            if x == 1 or x == self.w or y == 1 or y == self.h then
-                grid[x][y].outerWall = true
+            local prob = bsp_rng:random(100)
+            if prob <= 95 then
+                if x == 1 or x == self.w or y == 1 or y == self.h then
+                    grid[x][y].outerWall = true
+                end
             end
         end
     end
