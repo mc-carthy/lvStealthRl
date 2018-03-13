c73bc55d5e5f37125089b162fec3c0a2addf1c80
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-03-13 18:24:46 +0000

    Fix issue where only one half of BSP generation was working

--- src/map/bspBuilding.lua
+++ src/map/bspBuilding.lua
@@ -4,6 +4,8 @@ local bspBuilding = {}
 
 local bsp_rng = love.math.newRandomGenerator(os.time())
 
+local iteration = 0
+
 local function _createOuterWalls(self)
     for x = 1, self.w do
         self.grid[x] = {}
@@ -30,21 +32,19 @@ local function _createRoom(self, x, y, w, h)
             end
         end
     end
-    if self.numIterations < 8 then
-        self._splitRoom(self, x, y, w, h)
-        self.numIterations = self.numIterations + 1
-    end
+    self._splitRoom(self, x, y, w, h)
 end
 
 --[[
     x and y represent top-left corner coord
 --]] 
 local function _splitRoom(self, x, y, w, h, minRoomSize)
+    iteration = iteration + 1
     local minRoomSize = minRoomSize or 10
-    local prob = love.math.random()
+    local prob = bsp_rng:random(100)
     local splitH = nil
     
-    if prob > 0.5 then
+    if prob > 50 then
         splitH = true
     else
         splitH = false
@@ -64,19 +64,24 @@ local function _splitRoom(self, x, y, w, h, minRoomSize)
         max = w - minRoomSize
     end
 
-    if max < minRoomSize then 
+    local prob2 = bsp_rng:random(100)
+    -- TODO remove hard-coded values
+    if max < minRoomSize or iteration > 2 and prob2 < 35 then
         return
     end
 
     local split = love.math.random(minRoomSize, max)
 
-    -- TODO: Investigate why only the first instance of _createRoom seems to be getting called
     if splitH then
         self:_createRoom(x, y + split, w, h - split)
+        iteration = 0
         self:_createRoom(x, y, w, split)
+        iteration = 0
     else
         self:_createRoom(x + split, y, w - split, h)
+        iteration = 0
         self:_createRoom(x, y, split, h)
+        iteration = 0
     end
 
     return true
