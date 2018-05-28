c1d32db94fff116b2b4d5c080e4165ac751c5ba0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-05-28 22:24:53 +0100

    Add main menu screen

--- main.lua
+++ main.lua
@@ -2,6 +2,18 @@ local level = require('src.scenes.level1')
 
 DEBUG = false
 currentLevel = nil
+LOADING = false
+
+local alarmClock = love.graphics.newFont('assets/fonts/alarm clock.ttf', 64)
+local blackRain = love.graphics.newFont('assets/fonts/BLACK RAIN.ttf', 64)
+local bladeRunner = love.graphics.newFont('assets/fonts/BLADRMF_.ttf', 64)
+local chintzy = love.graphics.newFont('assets/fonts/chintzy.ttf', 64)
+local chintzys = love.graphics.newFont('assets/fonts/chintzys.ttf', 64)
+local crt = love.graphics.newFont('assets/fonts/Hacked CRT.ttf', 64)
+local lineBeam = love.graphics.newFont('assets/fonts/Linebeam.ttf', 64)
+local sewer = love.graphics.newFont('assets/fonts/sewer.ttf', 64)
+local mainFont = lineBeam
+local subFont = alarmClock
 
 function love.load()
     love.graphics.setBackgroundColor(0, 0, 0, 0)
@@ -18,12 +30,25 @@ function love.update(dt)
 end
 
 function love.draw()
+    local defaultFont = love.graphics.getFont()
     if currentLevel and currentLevel.draw then
         currentLevel.draw()
+    elseif currentLevel == nil then
+        love.graphics.setFont(mainFont)
+        love.graphics.setColor(0, 191, 191, 255)
+        local titleText = 'WORKING TITLE'
+        local titleTextWidth = mainFont:getWidth(titleText)
+        local titleTextHeight = mainFont:getHeight()
+        love.graphics.printf(titleText, love.graphics.getWidth() / 2 - titleTextWidth / 2, love.graphics.getHeight() / 2 - 200 - titleTextHeight / 2, titleTextWidth, 'left')
+        
+        love.graphics.setFont(subFont)
+        love.graphics.setColor(191, 0, 191, 255)
+        local subtitleText = 'PRESS SPACE TO BEGIN'
+        local subtitleTextWidth = subFont:getWidth(subtitleText)
+        local subtitleTextHeight = subFont:getHeight()
+        love.graphics.printf(subtitleText, love.graphics.getWidth() / 2 - subtitleTextWidth / 4, love.graphics.getHeight() / 2 - subtitleTextHeight / 4, subtitleTextWidth, 'left', 0, 0.5, 0.5)
     end
-    if currentLevel == nil then
-        love.graphics.printf(text (string), x (number), y (number), limit (number), align (AlignMode), r (number), sx (number), sy (number), ox (number), oy (number), kx (number), ky (number))
-    end
+    love.graphics.setFont(defaultFont)
 end
 
 function love.mousepressed(x, y, button, isTouch)
