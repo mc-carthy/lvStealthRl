f6d3bc39001864b48d4fe4192c3798a8d93ca324
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-07-11 23:47:55 +0100

    Display imported map on level

--- src/scenes/firstLevelState.lua
+++ src/scenes/firstLevelState.lua
@@ -1,11 +1,28 @@
 FirstLevelState = Class{ __includes = BaseState}
 
-function FirstLevelState.update(dt)
+CELL_SIZE = 20
+
+function FirstLevelState:enter(params)
+    self.map = params.map
+end
+
+function FirstLevelState:update(dt)
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
     end
 end
 
-function FirstLevelState.draw()
-    love.graphics.print('Level 1!', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+function FirstLevelState:draw()
+    for x = 1, #self.map do
+        for y = 1, #self.map[1] do
+            if compareTables(mapData[x][y],{ 0, 0, 0, 1 }) == true then
+                love.graphics.setColor(0.5, 0.5, 0.5, 1)
+            elseif compareTables(mapData[x][y],{ 0, 0, 1, 1 }) == true then
+                love.graphics.setColor(0, 0.75, 0.75, 1)
+            else
+                love.graphics.setColor(1, 1, 1, 1)
+            end
+            love.graphics.rectangle('fill', x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+        end
+    end
 end
--- src/utils/utils.lua
+++ src/utils/utils.lua
@@ -1,15 +1,25 @@
 function readMapFromImage(filePath)
     imageData = love.image.newImageData(filePath)
-    imageWidth = imageData:getWidth() - 1
-    imageHeight = imageData:getHeight() - 1
+    imageWidth = imageData:getWidth()
+    imageHeight = imageData:getHeight()
 
     mapData = {}
 
-    for x = 0, imageWidth do
+    for x = 1, imageWidth do
         mapData[x] = {}
-        for y = 0, imageHeight do
-            mapData[x][y] = { imageData:getPixel(x, y) }
+        for y = 1, imageHeight do
+            mapData[x][y] = { imageData:getPixel(x - 1, y - 1) }
             print('Colour at x:' .. x .. '-y:' .. y .. ' r: ' .. mapData[x][y][1] .. ' g: ' .. mapData[x][y][2] .. ' b: ' .. mapData[x][y][3] .. ' a: ' .. mapData[x][y][4])
         end
     end
+
+    return mapData
+end
+
+function compareTables(t1, t2)
+    for i = 1, #t1 do
+        if t2[i] == nil then return false end
+        if t1[i] ~= t2[i] then return false end
+    end
+    return true
 end
