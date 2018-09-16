9602c225ced98d80233e2439c8720a067b1926c8
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-17 00:48:26 +0100

    Add alert/caution timers

--- src/entities/enemy/states/alert.lua
+++ src/entities/enemy/states/alert.lua
@@ -9,13 +9,15 @@ function AlertState:enter(body)
     self.body.viewDist = alertViewDist
     self.body.viewAngle = math.rad(alertViewAngle)
     self.body.coneColour = alertConeColour
+    self.alertTimer = body.alertTimer or 100
 
     self.body.alertSfx:stop()
     self.body.alertSfx:play()
 end
 
 function AlertState:update(dt)
-    if not self.body:canSeePlayer() then
+    self.alertTimer = self.alertTimer - dt
+    if self.alertTimer < 0 then 
         self.body.stateMachine:change('caution', self.body)
     end
 end
--- src/entities/enemy/states/caution.lua
+++ src/entities/enemy/states/caution.lua
@@ -9,10 +9,15 @@ function CautionState:enter(body)
     self.body.viewDist = cautionViewDist
     self.body.viewAngle = math.rad(cautionViewAngle)
     self.body.coneColour = cautionConeColour
+    self.cautionTimer = body.cautionTimer or 100
 end
 
 function CautionState:update(dt)
+    self.cautionTimer = self.cautionTimer - dt
     if self.body:canSeePlayer() then
         self.body.stateMachine:change('alert', self.body)
+    end
+    if self.cautionTimer < 0 then
+        self.body.stateMachine:change('idle', self.body)
     end
 end
